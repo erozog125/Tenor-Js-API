fde959a7f9780ab13b625057d44c4d55468ee050
Edwin Rozo Gómez <erozog125@gmail.com>
2022-05-08 21:09:33 -0500

    Init Project

---
+++ app.js
@@ -0,0 +1,29 @@
+const inputGif = document.getElementById("input-gif");
+const mainContainer = document.getElementById("main-container");
+
+inputGif.addEventListener('keyup', customGif);
+
+function customGif(event) {
+
+   
+  const URL = "https://g.tenor.com/v1/search?"  
+  const key = "DMHFGEWPEFHD";
+  const query = `q=${event.target.value}`
+  const limit = "&limit=16";
+
+  fetch(`${URL}${query}&key=${key}${limit}`)
+  .then(response => response.json())
+  .then(data => createGif(data))
+}
+
+function createGif(data) {  
+  data.results.map( gif => {
+    const imgCard = document.createElement("img");
+    imgCard.classList.add('img-gif');
+    imgCard.src = gif.media[0].mediumgif.url;     
+    mainContainer.appendChild(imgCard);
+  });  
+}
+
+
+
