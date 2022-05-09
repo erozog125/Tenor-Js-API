e795157faf3cc0f3bee97179a379aaf981fab20d
Edwin Rozo Gómez <erozog125@gmail.com>
2022-05-09 13:09:54 -0500

    Limpiar Dom

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const mainContainer = document.getElementById("main-container");
 
 inputGif.addEventListener('keyup', customGif);
 
-function customGif(event) {
-
-   
+function customGif(event) {   
   const URL = "https://g.tenor.com/v1/search?"  
   const key = "DMHFGEWPEFHD";
   const query = `q=${event.target.value}`
-  const limit = "&limit=16";
+  const limit = "&limit=4";
 
   fetch(`${URL}${query}&key=${key}${limit}`)
   .then(response => response.json())
-  .then(data => createGif(data))
+  .then(data => createGif(data))  
 }
 
-function createGif(data) {  
+
+function createGif(data) {   
+  mainContainer.innerHTML = "";
   data.results.map( gif => {
     const imgCard = document.createElement("img");
     imgCard.classList.add('img-gif');
